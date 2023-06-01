764bba3a16e7a6f2a4a89364063e8bf29ea4edfd
AdhaNoer4 <adhanoerhidayah4@gmail.com>
2023-06-01 14:49:12 +0700

    04 OOP PHP Object Type

---
+++ object-type.php
@@ -0,0 +1,47 @@
+<?php  
+
+class Produk{
+    //property(variabel pada class)
+        public $judul, // public adalah visibility
+                $penulis ,
+                $penerbit,
+                $harga ;
+   
+    //function construct(adalah method yg dijalankan otomatis setiap kita membuat instance pada setiap kelas)
+    public function __construct($judul = "judul",$penulis= "penulis",$penerbit = "penerbit",$harga= 0){ //variabel pada parameter berbeda dengan property di class produk
+       //di terima constructor, dipakai untuk mengganti propertynya
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+
+    }
+
+    // Method (Function pada class)
+    public function getLabel(){
+        return "$this->penulis,$this->penerbit";
+    }
+}
+
+// Object Type
+class CetakInfoProduk{
+    public function cetak(Produk $produk){// menerima inputan object dari class produk 
+        $str = "{$produk->judul}|{$produk->getLabel()} (Rp. {$produk->harga})";
+        return $str;
+    }
+}
+
+
+
+//instance class produk
+$produk1 /* <- ini object*/ = new Produk("Naruto","Masashi kishimoto","Shonen Jump",30000);// dikirim ke constructor
+$produk2 = new Produk("Mobile Legends","Moonton","Byte Dance",50000);
+
+echo "Komik : " . $produk1->getLabel();
+echo "<br>";
+echo "Game : " . $produk2->getLabel();
+echo "<br>";
+
+// instance class CetakInfoProduk
+$infoProduk1 = new CetakInfoProduk();
+echo $infoProduk1->cetak($produk1);// cetak produk 1
