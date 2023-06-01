76163e1381cc5cc985dae44c59f460ee214c2823
AdhaNoer4 <adhanoerhidayah4@gmail.com>
2023-06-01 10:51:58 +0700

    01 OOP PHP Class & Object

---
+++ coba.php
@@ -0,0 +1,14 @@
+<?php 
+
+class Coba{
+
+}
+
+// membuat object/blueprint dari class coba
+$a = new Coba(); // <- ini adalah object
+$b = new Coba();
+$c = new Coba();
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
---
+++ produk.php
@@ -0,0 +1,14 @@
+<?php  
+
+// Studi kasus
+// Jualan Produk
+// komik 
+// game
+
+class Produk{
+
+}
+
+//object
+$produk1 = new Produk();
+$produk2 = new Produk();
